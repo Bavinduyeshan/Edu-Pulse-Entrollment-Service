2f96aaf411a0171443bde02f36c890bf33972493
Bavindu Yeshan <155254497+Bavinduyeshan@users.noreply.github.com>
2025-12-25 18:48:33 +0530

    initial commit

---
+++ src/main/java/com/Edupulse/EntrollmentService/EntrollmentServiceApplication.java
@@ -0,0 +1,13 @@
+package com.Edupulse.EntrollmentService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EntrollmentServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EntrollmentServiceApplication.class, args);
+	}
+
+}
